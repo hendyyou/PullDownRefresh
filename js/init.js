462106aa8d9c2542f91d719ad9783607de871030
dawncc <dawncc@live.com>
2013-03-03 17:10:11 +0800

    jqm用iscroll实现下拉刷新
    1、采用动态添加iscroll
    2、解决列表布局超出页面长度的BUG
    3、iscroll自适应页面

--- js/init.js
+++ js/init.js
@@ -1,91 +1,139 @@
-		var myScroll,
-			pullDownEl, pullDownOffset,
-			pullUpEl, pullUpOffset,
-			generatedCount = 0;
-
-			function pullDownAction () {
-	setTimeout(function () {	// <-- Simulate nameetwork congestion, remove setTimeout from production!
-		var el, li, i;
-		el = document.getElementById('thelist');
-
-		for (i=0; i<3; i++) {
-			li = document.createElement('li');
-			li.innerText = 'Generated row ' + (++generatedCount);
-			el.insertBefore(li, el.childNodes[0]);
+$(function() {
+	function fixed(elm) {
+		if (elm.data("iscroll-plugin")) {
+			return;
 		}
-		
-		myScroll.refresh();		// Remember to refresh when contents are loaded (ie: on ajax completion)
-	}, 1000);	// <-- Simulate network congestion, remove setTimeout from production!
-}
 
-function pullUpAction () {
-	setTimeout(function () {	// <-- Simulate network congestion, remove setTimeout from production!
-		var el, li, i;
-		el = document.getElementById('thelist');
+		elm.css({
+			overflow: 'hidden'
+		});
 
-		for (i=0; i<3; i++) {
-			li = document.createElement('li');
-			li.innerText = 'Generated row ' + (++generatedCount);
-			el.appendChild(li, el.childNodes[0]);
-		}
-		
-		myScroll.refresh();		// Remember to refresh when contents are loaded (ie: on ajax completion)
-	}, 1000);	// <-- Simulate network congestion, remove setTimeout from production!
-}
+         var barHeight = 0; // 页头页尾高度
+         var myScroll,
+         pullUpEl, pullUpOffset;
+         // 设置页头样式
+         var $header = elm.find('[data-role="header"]');
+         if ($header.length) {
+         	$header.css({
+         		"z-index": 1000,
+         		padding: 0,
+         		width: "100%"
+         	});
+         	barHeight += $header.height();
+         }
 
-function loaded() {
-	pullDownEl = document.getElementById('pullDown');
-	pullDownOffset = pullDownEl.offsetHeight;
-	pullUpEl = document.getElementById('pullUp');	
-	pullUpOffset = pullUpEl.offsetHeight;
-	
-	myScroll = new iScroll('wrapper', {
-		useTransition: true,
-		topOffset: pullDownOffset,
-		onRefresh: function () {
-			if (pullDownEl.className.match('loading')) {
-				pullDownEl.className = '';
-				pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Pull down to refresh...';
-			} else if (pullUpEl.className.match('loading')) {
-				pullUpEl.className = '';
-				pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Pull up to load more...';
-			}
-		},
-		onScrollMove: function () {
-			if (this.y > 5 && !pullDownEl.className.match('flip')) {
-				pullDownEl.className = 'flip';
-				pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Release to refresh...';
-				this.minScrollY = 0;
-			} else if (this.y < 5 && pullDownEl.className.match('flip')) {
-				pullDownEl.className = '';
-				pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Pull down to refresh...';
-				this.minScrollY = -pullDownOffset;
-			} else if (this.y < (this.maxScrollY - 5) && !pullUpEl.className.match('flip')) {
-				pullUpEl.className = 'flip';
-				pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Release to refresh...';
-				this.maxScrollY = this.maxScrollY;
-			} else if (this.y > (this.maxScrollY + 5) && pullUpEl.className.match('flip')) {
-				pullUpEl.className = '';
-				pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Pull up to load more...';
-				this.maxScrollY = pullUpOffset;
-			}
-		},
-		onScrollEnd: function () {
-			if (pullDownEl.className.match('flip')) {
-				pullDownEl.className = 'loading';
-				pullDownEl.querySelector('.pullDownLabel').innerHTML = 'Loading...';				
-				pullDownAction();	// Execute custom function (ajax call?)
-			} else if (pullUpEl.className.match('flip')) {
+         // 设置页尾样式
+         var $footer = elm.find('[data-role="footer"]');
+         if ($footer.length) {
+         	$footer.css({
+         		"z-index": 1500,
+         		padding: 0,
+         		width: "100%"
+         	});
+         	barHeight += $footer.height();
+         }
+
+         // 设置内容区域样式、高度
+         var $wrapper = elm.find('[data-role="content"]');
+         if ($wrapper.length) {
+         	$wrapper.css({
+         		"z-index": 1
+         	});
+         	$wrapper.height($(window).height() - barHeight);
+         	$wrapper.bind('touchmove', function (e) { e.preventDefault(); });
+         }
+         // 添加刷新提示
+         var $li = $("[data-role='listview']");
+         $li.after('<ul id="pullUp"><li class="pullUpIcon"></li><li class="pullUpLabel">上拉可以刷新...</li></ul>');
+         // 设置滚动区域
+         var scroller = elm.find('[data-iscroll="scroller"]').get(0);
+         if (!scroller) {
+         	$($wrapper.get(0)).children().wrapAll("<div data-iscroll='scroller'></div>");
+         }
+         
+         // 添加滚动条
+         pullUpEl = document.getElementById('pullUp');
+         pullUpOffset = pullUpEl.offsetHeight;
+         var iscroll = new iScroll($wrapper.get(0), {
+
+         	hScroll        : false,
+         	vScroll        : true,
+         	hScrollbar     : false,
+         	vScrollbar     : false,
+         	fixedScrollbar : true,
+         	fadeScrollbar  : false,
+         	hideScrollbar  : true,
+         	bounce         : true,
+         	momentum       : true,
+         	lockDirection  : true,
+         	checkDOMChanges: true,
+         	onBeforeScrollStart: function (e) {
+         		var target = e.target;
+         		while (target.nodeType != 1) target = target.parentNode;
+
+                 // 解决添加iscroll后输入框的问题
+                 if (target.tagName != 'SELECT'&& target.tagName !='option'&& target.tagName !='option' && target.tagName != 'INPUT' && target.tagName != 'TEXTAREA')
+                 	e.preventDefault();
+                 e.stopPropagation();
+             },
+             onRefresh: function () {
+             	if (pullUpEl.className.match('loading')) {
+             		pullUpEl.className = '';
+             		pullUpEl.querySelector('.pullUpLabel').innerHTML = '上拉可以刷新...';
+             	}
+             },
+             onScrollMove: function () {
+             	this.minScrollY = 0;
+             	if (this.y < (this.maxScrollY - 5) && !pullUpEl.className.match('flip')) {
+             		pullUpEl.className = 'flip';
+             		pullUpEl.querySelector('.pullUpLabel').innerHTML = '松开可以刷新...';
+             		this.maxScrollY = this.maxScrollY;
+             	} else if (this.y > (this.maxScrollY + 5) && pullUpEl.className.match('flip')) {
+             		pullUpEl.className = '';
+             		pullUpEl.querySelector('.pullUpLabel').innerHTML = '加载更多...';
+             		this.maxScrollY = pullUpOffset;
+             	}
+             },
+             onScrollEnd: function () {
+			// Execute custom function (ajax call?)
+			if (pullUpEl.className.match('flip')) {
 				pullUpEl.className = 'loading';
-				pullUpEl.querySelector('.pullUpLabel').innerHTML = 'Loading...';				
+				pullUpEl.querySelector('.pullUpLabel').innerHTML = '加载中...';				
 				pullUpAction();	// Execute custom function (ajax call?)
 			}
 		}
 	});
-
-setTimeout(function () { document.getElementById('wrapper').style.left = '0'; }, 800);
+//加载数据
+function pullUpAction () {
+	setTimeout(function () {	// <-- Simulate network congestion, remove setTimeout from production!
+		loadData();
+		myScroll.refresh();		// Remember to refresh when contents are loaded (ie: on ajax completion)
+	}, 1000);	// <-- Simulate network congestion, remove setTimeout from production!
 }
 
-document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+function loadData(){
+	var el, li, i;
+	el = $("#thelist");
+	console.log($("#thelist li").length);
+	li = $("#thelist li").first().html();
+	console.log(li);
+	for (i=0; i<10; i++) {
+		// el.append(li);
+		el.append("<li><a href='#'><img src='images/item4.jpg'/><h3> 送限量鼠标垫</h3><p>（送限量版鼠标垫）</p></a></li>")
+	}
+	// el.trigger( "create" );
+	el.listview("refresh");
+	console.log($("#thelist li").length);
+}
+elm.data("iscroll-plugin", iscroll);
 
-document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);
+window.setTimeout(function(){iscroll.refresh();}, 200);
+}
+$('[data-role="page"][data-iscroll="enable"]').bind("pageshow", function() {
+	fixed($(this));
+});
+if ($.mobile.activePage.data("iscroll") == "enable") {
+	fixed($.mobile.activePage);
+}
+
+});
